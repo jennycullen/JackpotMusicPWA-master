13da6a4fe22dde7841d44b5b0ad0e6510ab80628
root <xheni.cullen@gmail.com>
2023-01-04 17:39:00 -0800

    auth-routes.ts updated

--- src/app/shared/routes/auth-routes.ts
+++
@@ -1,5 +0,0 @@
-export const AUTH_ROOT_ROUTE: string[] = ['auth'];
-
-export const AUTH_SIGN_UP_ROUTE = [...AUTH_ROOT_ROUTE, 'signup'];
-export const AUTH_LOGIN_ROUTE = [...AUTH_ROOT_ROUTE, 'login'];
-export const AUTH_FORGOT_PASSWORD_ROUTE = [...AUTH_ROOT_ROUTE, 'forgot-password'];
